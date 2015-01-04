3a8e57ecdb6925cfc55c2c71af48d312e869a3b9
Emanuel H. Farias <emanuelfarias@outlook.com>
2015-01-04 16:38:20 -0200

    add linux/mac support

--- README.md
+++ README.md
@@ -17,9 +17,11 @@ It will download and install the last Sublime Text 2 and generate a gnome3 launc
 On Mac this is not necessary, just download the .dmg file (in the main website) and install.
 
 ### Setup Sublime configs and plugins
+First install Package Control: https://packagecontrol.io. Then, run:
 ```sh
 ruby go.rb
 ```
+It will automatic install Sublime plugins and set configs. Reload Sublime to take effect.
 
 ## Uninstalling Sublime Text 2 on Debian
 ```sh
--- go.rb
+++ go.rb
@@ -1,11 +1,25 @@
-require 'FileUtils'
+require 'fileutils'
+require 'rbconfig'
 
-# ==== Sublime Text 2 (Preferences and Plugins) ====
+def os
+    @os ||= (
+      host_os = RbConfig::CONFIG['host_os']
+      case host_os
+      when /darwin|mac os/
+        :macosx
+      when /linux/
+        :linux
+      end
+    )
+end
+
 sublime_pref_file = 'Preferences.sublime-settings'
 sublime_plug_file = 'Package Control.sublime-settings'
 
 sublime_orig = "sublime/"
-sublime_dest = ENV['HOME'] + '/Library/Application Support/Sublime Text 2/Packages/User/'
+sublime_mac = '/Library/Application Support/Sublime Text 2/Packages/User/'
+sublime_linux = '/.config/sublime-text-2/Packages/User/'
+sublime_dest = ENV['HOME'] + (os == :macosx ? sublime_mac : sublime_linux)
 
 # Copy sublime preferences
 FileUtils.cp(sublime_orig + sublime_pref_file, sublime_dest + sublime_pref_file)
