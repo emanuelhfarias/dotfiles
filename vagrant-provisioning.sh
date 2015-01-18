321245b17cdeee227f60a65bec77992c510046a8
Emanuel H. Farias <emanuelfarias@outlook.com>
2015-01-18 02:15:44 -0200

    fix rbenv, remove rails

--- vagrant-provisioning.sh
+++ vagrant-provisioning.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 
 # This vagrant shell is provisioning:
-# rbenv + ruby-build + ruby 2.2.0 + bundler + Rails (last version)
+# rbenv + ruby-build + ruby 2.2.0 + bundler
 
-# PS: The box must be Debian/Ubuntu based
-# See more on RailsCast #292
+# PS1: The box must be Debian/Ubuntu based.
+# PS2: This script is running as normal user (with sudo), NOT root. (privileged: false on Vagrantfile)
+# PS3: Ignore the error messages about stdin.
+# PS4: This script still doesn't support ZSH, only Bash.
+# See the manual installation on RailsCast #292.
 
-sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev git libsqlite3-dev &&
+BASH_RC="$HOME/.bashrc" # rbenv is only add here when using Ubuntu, otherwise, ~/.bash_profile
+RUBY_VERSION=2.2.0 # Choose your favorite version. List all available versions: rbenv install -l
 
-# installing rbenv
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv &&
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile &&
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile &&
-source ~/.bash_profile &&
+echo "Updating repositories and installing build-essential and others dependencies ..."
+sudo apt-get update &&
+sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev &&
+sudo apt-get install -y zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev &&
+sudo apt-get install -y git libsqlite3-dev &&
+echo "build-essential - OK"
 
-# installing ruby-build
-cd ~
-git clone https://github.com/sstephenson/ruby-build.git &&
-cd ruby-build/
+echo "installing rbenv ..."
+git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv &&
+EXPORT_PATH="export PATH='${HOME}/.rbenv/bin:$PATH'"
+echo $EXPORT_PATH >> $BASH_RC &&
+echo 'eval "$(rbenv init -)"' >> $BASH_RC &&
+eval $EXPORT_PATH
+eval "$(rbenv init -)"
+echo "rbenv - OK"
+
+echo "installing ruby-build ..."
+cd $HOME && git clone https://github.com/sstephenson/ruby-build.git $HOME/.ruby-build && &&
+cd .ruby-build/
 sudo ./install.sh &&
+echo "ruby-build - OK"
 
-# compiling and installing Ruby 2.2.0 via rbenv + ruby-build
-rbenv install 2.2.0 &&
+echo "compiling Ruby ..."
 rbenv rehash &&
-rbenv global 2.2.0 &&
+rbenv install $RUBY_VERSION &&
+rbenv rehash &&
+rbenv global $RUBY_VERSION &&
+echo "Ruby - OK"
 
-# installing bundler
+echo "installing bundler ..."
 gem install bundler &&
+rbenv rehash &&
+echo "bundler - OK"
 
-# installing
-gem install rails &&
-rbenv rehash
+#echo "installing rails last version ..."
+#gem install rails &&
+#rbenv rehash
+#echo "Rails - OK"
+
+echo "Provisioning Complete!!!"
